660104d00e418373de50e95f348c3a0748fe4bae
avrora4dvar <37673736+avrora4dvar@users.noreply.github.com>
2018-09-14 19:12:04 -0700

    Add files via upload

--- Python/plt1_plume_profile.py
+++ Python/plt1_plume_profile.py
@@ -23,14 +23,14 @@ import datetime
 import matplotlib.ticker as ticker
 
 
-mat=sio.loadmat('V:\ipasmans\TSz_plume.mat',squeeze_me=True,struct_as_record=False)
+mat=sio.loadmat('E:\ipasmans\TS_plume_exp26.mat',squeeze_me=True,struct_as_record=False)
 
 #%% Load data
 
 plt.close('all')
 matplotlib.rcParams['font.family']='Arial'
 plt.rcParams.update({'font.size':8})
-fig=plt.figure(figsize=(5.61*.9,8.92*.4))
+fig=plt.figure(figsize=(5.61,8.92*.39))
 
 #%% 
 
@@ -38,10 +38,10 @@ ax=[]
 labels=['a)','b)']
 for iax in np.arange(0,2):
     ax1=fig.add_subplot(1,2,iax+1)
-    ax1.set_ylim(200,0)
+    ax1.set_ylim(150,0)
     if iax==0:
         ax1.set_ylabel('Depth [m]')
-        ax1.yaxis.set_major_locator(ticker.MultipleLocator(50))
+        ax1.yaxis.set_major_locator(ticker.MultipleLocator(25))
     else:
         ax1.yaxis.set_major_formatter(ticker.NullFormatter())
     ax1.text(.02,.94,s=labels[iax],transform=ax1.transAxes)
@@ -51,12 +51,12 @@ for iax in np.arange(0,2):
 #%% Plot salinity
     
 ax1=ax[0]
-ax1.set_xlabel('Salinity [ppt]')
+ax1.set_xlabel('Salinity')
 ax1.plot(mat['cr'].salt[:,0],mat['cr'].depth,'k-',label='Obs (plume)')
-ax1.plot(mat['cr'].salt[:,1],mat['cr'].depth,'b-',label='No DA (plume)')
+ax1.plot(mat['cr'].salt[:,1],mat['cr'].depth,'-',color='dodgerblue',label='Long Free (plume)')
 ax1.plot(mat['nocr'].salt[:,0],mat['nocr'].depth,'k--',label='Obs (no plume)')
-ax1.plot(mat['nocr'].salt[:,1],mat['nocr'].depth,'b--',label='No DA (no plume)')
-leg=ax1.legend(loc='lower left')
+ax1.plot(mat['nocr'].salt[:,1],mat['nocr'].depth,'--',color='dodgerblue',label='Long Free (no plume)')
+leg=ax1.legend(loc='lower left',bbox_to_anchor=(-.05,.02))
 leg.get_frame().set_alpha(1)
 
 #%% Plot temperature
@@ -64,12 +64,13 @@ leg.get_frame().set_alpha(1)
 ax1=ax[1]
 ax1.set_xlabel(u'Temperature [\u00B0C]')
 ax1.plot(mat['cr'].temp[:,0],mat['cr'].depth,'k-',label='Obs (plume)')
-ax1.plot(mat['cr'].temp[:,1],mat['cr'].depth,'b-',label='No DA (plume)')
+ax1.plot(mat['cr'].temp[:,1],mat['cr'].depth,'-',color='dodgerblue',label='Long Free (plume)')
 ax1.plot(mat['nocr'].temp[:,0],mat['nocr'].depth,'k--',label='Obs (no plume)')
-ax1.plot(mat['nocr'].temp[:,1],mat['nocr'].depth,'b--',label='No DA (no plume)')
-leg=ax1.legend(loc='lower right')
+ax1.plot(mat['nocr'].temp[:,1],mat['nocr'].depth,'--',color='dodgerblue',label='Long Free (no plume)')
+leg=ax1.legend(loc='lower left',bbox_to_anchor=(.3,.02))
 leg.get_frame().set_alpha(1)
 
 #%% Save
 
+fig.subplots_adjust(top=.98,left=.1,right=.98)
 fig.savefig('profile_plume.pdf')
--- Python/plt1_wind_salt.py
+++ Python/plt1_wind_salt.py
@@ -16,6 +16,7 @@ from datetime import datetime, timedelta
 import imp
 from matplotlib import animation
 from matplotlib import ticker
+from matplotlib import dates as dates
 import matplotlib
 import seawater as sw
 imp.reload(plotter)
@@ -25,17 +26,17 @@ matplotlib.rcParams['font.family']='Arial'
 
 plt.close('all')
 #fig=plt.figure(figsize=(5,3))
-fig=plt.figure(figsize=(3.66,8.92*.5))
+fig=plt.figure(figsize=(5.61,8.92*.5))
 
 #%%
 
-mat=sio.loadmat('V:\ipasmans\wind_stress.mat',squeeze_me=True,struct_as_record=False)
+mat=sio.loadmat('E:\ipasmans\wind_stress.mat',squeeze_me=True,struct_as_record=False)
 wind=mat['wind']
 
 
 wind.t=wind.t-366.
 dateRef=datetime(2005,1,1).toordinal()
-tLim=np.array([2380,2392,2410,2434])+dateRef
+tLim=np.array([2380,2392,2413,2434])+dateRef
 
 #%% Plot
 
@@ -44,25 +45,28 @@ labels=['a)','b)','c)']
 for iax in np.arange(0,2):
     ax1=fig.add_subplot(2,1,iax+1)
     ax1.grid(color=(.7,.7,.7),linestyle='--',linewidth=.5)
-    ax1.xaxis.set_major_locator(ticker.FixedLocator(np.arange(wind.t[0],wind.t[-1]+.01,12)))
+    ax1.xaxis.set_major_locator(dates.MonthLocator())
     if iax==1:
         ax1.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x,pos:plotter.num2time(x).strftime('%m/%d')))
         ax1.set_xlabel('2011')
     else:
         ax1.xaxis.set_major_formatter(ticker.NullFormatter())
-    ax1.text(.02,.93,s=labels[iax],transform=ax1.transAxes)
-    ax1.set_xlim(tLim[0],tLim[-1])
+    ax1.text(.02,.92,s=labels[iax],transform=ax1.transAxes)
+    ax1.set_ylim(24.,34.)
+    ax1.set_xlim(datetime(2011,5,1),datetime(2011,10,1))
     ax.append(ax1)
 
 #%% Plot wind
 iax=0
+#wind.vf=plotter.running_mean(wind.t,wind.v,1.)
+#wind.vf=plotter.running_mean(wind.t,wind.vf,1.)
 ax[iax].plot(wind.t,wind.v,linewidth=1,color='b')
-ax[iax].plot([tLim[0],tLim[-1]],[0,0],color='k',linewidth=1)
+ax[iax].plot([datetime(2011,1,1),datetime(2011,12,1)],[0,0],color='k',linewidth=1)
 ax[iax].plot([tLim[1],tLim[1]],[-10,10],color='k',linewidth=1,linestyle='--')
 ax[iax].plot([tLim[2],tLim[2]],[-10,10],color='k',linewidth=1,linestyle='--')
 
-ax[iax].text(tLim[0]+.5,8,'{0}\n{1}'.format('down-','welling'))
-ax[iax].text(tLim[0]+.5,-9,'upwelling')
+ax[iax].text(.7,.9,s='{0}'.format('downwelling'),transform=ax[iax].transAxes)
+ax[iax].text(.7,.04,'upwelling',transform=ax[iax].transAxes)
 
 ax[iax].yaxis.set_major_locator(ticker.MultipleLocator(.05))
 ax[iax].set_ylim(-.15,.15)
@@ -70,25 +74,29 @@ ax[iax].set_ylabel('{0} {1}'.format(r'Meridional wind stress',r'[$\mathrm{Nm^{-2
 
 #%% Plot No DA, observations salinity NH10
 
-mat=sio.loadmat('V:\ipasmans\\NH10_ana.mat',squeeze_me=True,struct_as_record=False)
+mat=sio.loadmat('E:\ipasmans\\NH10_exp26.mat',squeeze_me=True,struct_as_record=False)
 mat['obs'].t=mat['obs'].t-366.
 
 #Apply running mean
 mat['obs'].saltf=plotter.running_mean(mat['obs'].t,mat['obs'].salt[0,:],1.)
-for mod1 in mat['model']:
-    mod1.saltf=plotter.running_mean(mat['obs'].t,mod1.salt[0,:],1.)
+mat['model'].saltf=plotter.running_mean(mat['model'].t,mat['model'].salt[0,:],1.)
+mat['obs'].saltf=plotter.running_mean(mat['obs'].t,mat['obs'].saltf,1.)
+mat['model'].saltf=plotter.running_mean(mat['model'].t,mat['model'].saltf,1.)
 
 ax1=ax[1]
-ax1.plot(mat['obs'].t,mat['obs'].saltf,'k-',label='Obs')
-ax1.plot(mat['obs'].t,mat['model'][3].saltf,'b-',label='No DA')
-ax1.set_ylabel('Salinity [ppt]')
-leg=ax1.legend(loc='lower right')
+ax1.plot(mat['obs'].t,mat['obs'].saltf,'k-',label='NH10')
+ax1.plot(mat['obs'].t,mat['model'].saltf,'-',color='dodgerblue',label='Long Free')
+ax1.plot([tLim[1],tLim[1]],[-10,40],color='k',linewidth=1,linestyle='--')
+ax1.plot([tLim[2],tLim[2]],[-10,40],color='k',linewidth=1,linestyle='--')
+ax1.set_ylim(21.5,34)
+ax1.set_ylabel('Salinity')
+leg=ax1.legend(loc='lower left',bbox_to_anchor=(.71,.01))
 leg.get_frame().set_alpha(1)
 
 #%%
 
 
-fig.subplots_adjust(left=.2,bottom=.1,top=.98,right=.96,hspace=.08)
+fig.subplots_adjust(left=.15,bottom=.1,top=.98,right=.95,hspace=.08)
 
 #fig.savefig('wind.png',figsize=(5,2.5),dpi=500)
 fig.savefig('wind.pdf')
